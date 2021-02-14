eb8c6b1bd1ba9eb312783445fdb89327fb52265c
Apoorva Odugoudar <aeemo@users.noreply.github.com>
2021-02-14 20:37:06 +0530

    files added

---
+++ src/settings.5f623.js
@@ -0,0 +1,35 @@
+window._CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "default",
+        "round",
+        "fire"
+    ],
+    collisionMatrix: [
+        [
+            false
+        ],
+        [
+            false,
+            false,
+            true
+        ],
+        [
+            false,
+            true,
+            false
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/game.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "202c4",
+        main: "09c68"
+    }
+};
